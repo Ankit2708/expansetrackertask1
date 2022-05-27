d4c7fd6a633efe3007ab193102dba57207b2da5c
Ankit2708 <shivhare.ankit476@gmail.com>
2022-05-27 14:08:31 +0530

    expansebackendfeature

--- front/home.js
+++ front/home.js
@@ -1,4 +1,38 @@
 const expanse=document.getElementById('expanse_form')
+const expanse_item_count=document.getElementById('expanse_item_count')
+let count=1
+document.addEventListener('DOMContentLoaded',async()=>{
+    const expanse_item_count=document.getElementById('expanse_item_count')
+    var body=document.getElementsByTagName("BODY")[0]
+    const expanse=document.getElementById('expanse')
+    const expanse_form=document.getElementById('expanse_form')
+    let premium=true
+    if(premium){
+        expanse_form.style.backgroundColor = "rgba(0, 0, 0, 0.855)"
+        expanse_form.style.color = "white"
+        expanse.style.backgroundColor = "rgba(0, 0, 0, 0.855)"
+        body.style.backgroundImage  = "url('https://d2gg9evh47fn9z.cloudfront.net/1600px_COLOURBOX24284715.jpg')"
+        expanse.style.color = "white"
+    }
+    console.log('dom loaded')
+    const token=localStorage.getItem('token')
+    await axios.get('http://localhost:3000/getexpanse',{
+        headers:{"Authorization":token}
+    }).then(data=>{
+        console.log(data.data)
+        data=data.data
+        for(let i=0;i<data.length;i++){
+            expanse_item_count.innerHTML=expanse_item_count.innerHTML+`<div class="expanse_item">
+            <p>${count}</p>
+            <p>${data[i].expanseAmount}</p>
+            <p>${data[i].description}</p>
+            <p>${data[i].category}</p>
+            </div>`
+            count++
+        }
+    })
+
+})
 expanse.addEventListener('submit',(e)=>{
     e.preventDefault()
     expanseAmount=e.target.money.value;
@@ -14,5 +48,17 @@ expanse.addEventListener('submit',(e)=>{
         headers:{"Authorization":token}
     }).then((data)=>{
         console.log(data)
+        e.target.money.value='';
+        e.target.description.value='';
+        e.target.category.value='';
+        const data=data.data.expanse;
+        expanse_item_count.innerHTML=expanse_item_count.innerHTML+`<div class="expanse_item">
+        <p>${count}</p>
+        <p>${data[i].expanseAmount}</p>
+        <p>${data[i].description}</p>
+        <p>${data[i].category}</p>
+        </div>`
+        count++
+        
     }).catch(err=>console.log(err))
 })
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,12 @@
 const express=require('express')
+
 const userController=require('../controller/user')
+const expanseController=require('../controller/expanse')
 const authmiddleware=require('../middleware/auth')
 const router=express.Router()
 router.post('/signUp',userController.signUp)
 router.post('/login',userController.login)
+router.post('/addExpanse',authmiddleware.authenticate,expanseController.addExpanse)
+router.get('/getExpanse',authmiddleware.authenticate,expanseController.getExpanse)
+router.delete('/deleteExpanse/:expanseId',authmiddleware.authenticate,expanseController.deleteExpanse)
 module.exports=router;
